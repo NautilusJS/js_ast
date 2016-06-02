acaf5efd999de97f8419378aab77d961c4782398
mailmindlin <liamttlyrox@gmail.com>
2016-06-02 12:36:45 -0400

    Added nice character class

---
+++ src/com/mindlin/jsast/impl/parser/Characters.java
@@ -0,0 +1,23 @@
+package com.mindlin.jsast.impl.parser;
+
+public final class Characters {
+	public static final char NULL = '\0',
+		SOH = '\u0001',
+		STX = '\u0002',
+		ETX = '\u0003',
+		EOT = '\u0004',
+		ENQ = '\u0005',
+		ACK = '\u0006',
+		BEL = '\u0007',
+		BS  = '\u0008',
+		TAB = '\u0009',
+		LF  = '\n',
+		VT  = '\u000b',
+		FF  = '\u000c',
+		CR  = '\r',
+		SO  = '\u000e',
+		SI  = '\u000f'
+		
+		//SPACE = '\u0020',
+		;
+}
