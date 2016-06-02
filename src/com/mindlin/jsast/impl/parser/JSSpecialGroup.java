4674a948e902f558b71d9effaabe527a1342a3ec
mailmindlin <liamttlyrox@gmail.com>
2016-06-02 12:36:04 -0400

    Documented

--- src/com/mindlin/jsast/impl/parser/JSSpecialGroup.java
+++ src/com/mindlin/jsast/impl/parser/JSSpecialGroup.java
@@ -1,7 +1,15 @@
 package com.mindlin.jsast.impl.parser;
 
 public enum JSSpecialGroup {
+	@Deprecated
 	SEMICOLON,
+	/**
+	 * Denotes the end of a line. This is a semicolon in an ideal world,
+	 * but it could be a newline or something that isn't escaped.
+	 */
 	EOL,
+	/**
+	 * Denotes the end of the file. Not that this doesn't actually map to a character in the file.
+	 */
 	EOF;
 }
--- src/com/mindlin/jsast/impl/parser/NumericBase.java
+++ src/com/mindlin/jsast/impl/parser/NumericBase.java
@@ -5,11 +5,15 @@ public enum NumericBase {
 	OCTAL(8),
 	DECIMAL(10),
 	HEXDECIMAL(16);
+	static final char[] chars = "0123456789ABCDEF".toCharArray();
 	final int exponent;
 	NumericBase(int exponent) {
 		this.exponent = exponent;
 	}
 	public int getExponent() {
 		return exponent;
+	}
+	public boolean isDigit(char c) {
+		return false;
 	}
 }
