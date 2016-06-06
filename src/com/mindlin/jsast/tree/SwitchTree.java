67ad139c6fa78cc5c9d603f6aee1e6d8d8f04f14
mailmindlin <liamttlyrox@gmail.com>
2016-06-06 12:33:38 -0400

    Added override tags

--- src/com/mindlin/jsast/tree/SwitchTree.java
+++ src/com/mindlin/jsast/tree/SwitchTree.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface SwitchTree extends StatementTree {
 	List<? extends CaseTree> getCases();
 	ExpressionTree getExpression();
+	@Override
 	default Tree.Kind getKind() {
 		return Tree.Kind.SWITCH;
 	}
