f4a25c7e93fb19888065210326fbabf3d3078209
mailmindlin <liamttlyrox@gmail.com>
2016-06-09 15:04:39 -0400

    Better null checking in UnexpectedTokenException

--- src/com/mindlin/jsast/exception/JSUnexpectedTokenException.java
+++ src/com/mindlin/jsast/exception/JSUnexpectedTokenException.java
@@ -6,7 +6,7 @@ import com.mindlin.jsast.impl.lexer.TokenKind;
 public class JSUnexpectedTokenException extends JSSyntaxException {
 	private static final long serialVersionUID = -5669873573919357134L;
 	public JSUnexpectedTokenException(Token token) {
-		super("Unexpected token " + token, token.getStart());
+		super("Unexpected token " + token, token == null ? -1 : token.getStart());
 	}
 	public JSUnexpectedTokenException(Token token, TokenKind expectedKind) {
 		super("Unexpected token " + token + " (expected kind: " + expectedKind + ")", token.getStart());
