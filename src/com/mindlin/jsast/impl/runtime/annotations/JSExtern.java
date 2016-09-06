89b33f4890eb1a1e73849197c6aae404e6fd7eb2
mailmindlin <liamttlyrox@gmail.com>
2016-09-06 14:27:36 -0400

    Added object descriptors to JSExtern

--- src/com/mindlin/jsast/impl/runtime/annotations/JSExtern.java
+++ src/com/mindlin/jsast/impl/runtime/annotations/JSExtern.java
@@ -2,4 +2,7 @@ package com.mindlin.jsast.impl.runtime.annotations;
 
 public @interface JSExtern {
 	String name() default "";
+	boolean configurable() default false;
+	boolean enumerable() default false;
+	boolean writable() default false;
 }
