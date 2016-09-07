d2c9fc89b5b8ab483963060af93b07d046b11fe2
mailmindlin <liamttlyrox@gmail.com>
2016-09-06 23:22:31 -0400

    Implemented VariableTree, TypeTree

--- src/com/mindlin/jsast/tree/TypeTree.java
+++ src/com/mindlin/jsast/tree/TypeTree.java
@@ -1,9 +1,9 @@
 package com.mindlin.jsast.tree;
 
-public interface TypeTree extends Tree {
-	boolean isOptional();
+public interface TypeTree extends IdentifierTree {
 	boolean isFunction();
 	boolean isIndex();
+	boolean isArray();
 	/**
 	 * The variable or return type
 	 * @return
