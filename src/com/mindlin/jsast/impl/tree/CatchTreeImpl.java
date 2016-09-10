1170fd8b8c1a1ad3f01429a0da0ffdc976fd739c
mailmindlin <liamttlyrox@gmail.com>
2016-09-09 20:31:39 -0400

    Implemented trees

---
+++ src/com/mindlin/jsast/impl/tree/CatchTreeImpl.java
@@ -0,0 +1,34 @@
+package com.mindlin.jsast.impl.tree;
+
+import com.mindlin.jsast.tree.BlockTree;
+import com.mindlin.jsast.tree.CatchTree;
+import com.mindlin.jsast.tree.IdentifierTree;
+import com.mindlin.jsast.tree.TypeTree;
+
+public class CatchTreeImpl extends AbstractTree implements CatchTree {
+	BlockTree block;
+	IdentifierTree param;
+	TypeTree type;
+	public CatchTreeImpl(long start, long end, BlockTree block, IdentifierTree param, TypeTree type) {
+		super(start, end);
+		this.block = block;
+		this.param = param;
+		this.type = type;
+	}
+
+	@Override
+	public BlockTree getBlock() {
+		return block;
+	}
+
+	@Override
+	public IdentifierTree getParameter() {
+		return param;
+	}
+
+	@Override
+	public TypeTree getType() {
+		return type;
+	}
+
+}
