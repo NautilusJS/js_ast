7b480f17c43b47dd736231ade3c9563e75786952
mailmindlin <liamttlyrox@gmail.com>
2016-09-13 23:26:14 -0400

    Added test suite for all tests

---
+++ test/com/mindlin/jsast/impl/AllTests.java
@@ -0,0 +1,11 @@
+package com.mindlin.jsast.impl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CharacterStreamTest.class, JSKeywordTest.class, JSLexerTest.class, ToStringTest.class })
+public class AllTests {
+
+}
