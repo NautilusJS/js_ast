a881a5e7b57dff3078a083e7488cdbe566436e5b
mailmindlin <liamttlyrox@gmail.com>
2016-09-16 23:07:42 -0400

    Added standard predicates for commonly matched groups

---
+++ src/com/mindlin/jsast/impl/parser/TokenPredicate.java
@@ -0,0 +1,19 @@
+package com.mindlin.jsast.impl.parser;
+
+import java.util.function.Predicate;
+
+import com.mindlin.jsast.impl.lexer.Token;
+
+/**
+ * Commonly used predicates for matching certain classes of tokens
+ * @author mailmindlin
+ */
+public class TokenPredicate {
+	public static Predicate<Token> VARIABLE_START = t->(t.isKeyword() && (t.getValue() == JSKeyword.VAR || t.getValue() == JSKeyword.LET || t.getValue() == JSKeyword.CONST));
+	public static Predicate<Token> LET_OR_CONST = t->(t.isKeyword() && (t.getValue() == JSKeyword.LET || t.getValue() == JSKeyword.CONST));
+	public static Predicate<Token> IN_OR_OF = t->(t.isKeyword() && (t.getValue() == JSKeyword.IN || t.getValue() == JSKeyword.OF));
+	/**
+	 * TokenPredicate can't be instantiated
+	 */
+	private TokenPredicate() {}
+}
