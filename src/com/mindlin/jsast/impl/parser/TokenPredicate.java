5e3bd9b2195ae62e1b7ecab4dc64e7cb95b7a855
mailmindlin <liamttlyrox@gmail.com>
2016-09-20 22:08:49 -0400

    Added predicate for parameter starts
    Detects if a parmeter could have type information (optionality, a type
    declaration, or a default value).

--- src/com/mindlin/jsast/impl/parser/TokenPredicate.java
+++ src/com/mindlin/jsast/impl/parser/TokenPredicate.java
@@ -12,6 +12,7 @@ public class TokenPredicate {
 	public static Predicate<Token> VARIABLE_START = t->(t.isKeyword() && (t.getValue() == JSKeyword.VAR || t.getValue() == JSKeyword.LET || t.getValue() == JSKeyword.CONST));
 	public static Predicate<Token> LET_OR_CONST = t->(t.isKeyword() && (t.getValue() == JSKeyword.LET || t.getValue() == JSKeyword.CONST));
 	public static Predicate<Token> IN_OR_OF = t->(t.isKeyword() && (t.getValue() == JSKeyword.IN || t.getValue() == JSKeyword.OF));
+	public static Predicate<Token> PARAMETER_TYPE_START = t->(t.isOperator() && (t.getValue() == JSOperator.QUESTION_MARK || t.getValue() == JSOperator.COLON || t.getValue() == JSOperator.ASSIGNMENT));
 	public static Predicate<Token> TYPE_CONTINUATION = t->(t.isOperator() && (t.getValue() == JSOperator.BITWISE_OR || t.getValue() == JSOperator.BITWISE_AND));
 	/**
 	 * TokenPredicate can't be instantiated
