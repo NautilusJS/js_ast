acc343b3e84a2b02b3af64bbe858d04b531d8146
mailmindlin <liamttlyrox@gmail.com>
2016-09-20 20:11:06 -0400

    Ignoring ToStringTest

--- test/com/mindlin/jsast/impl/ToStringTest.java
+++ test/com/mindlin/jsast/impl/ToStringTest.java
@@ -2,6 +2,7 @@ package com.mindlin.jsast.impl;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.mindlin.jsast.impl.tree.ForEachLoopTreeImpl;
@@ -9,11 +10,12 @@ import com.mindlin.jsast.tree.ForEachLoopTree;
 
 public class ToStringTest {
 	
+	@Ignore
 	@Test
 	public void test() {
 		ForEachLoopTree loop = new ForEachLoopTreeImpl(0, 1, true, null, null, null);
 		System.out.println(loop.toString());
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 	
 }
