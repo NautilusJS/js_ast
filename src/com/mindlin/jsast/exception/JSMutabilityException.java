d1590f64df558cbc0048ca1cbc5f13accebbc58b
mailmindlin <liamttlyrox@gmail.com>
2017-01-11 23:36:52 -0500

    Create JSMutabilityException
    Thrown on attempted modification of immutable data structure.

---
+++ src/com/mindlin/jsast/exception/JSMutabilityException.java
@@ -0,0 +1,10 @@
+package com.mindlin.jsast.exception;
+
+public class JSMutabilityException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 546157737018489241L;
+	
+}
