3acf5acd885d559a6cd4db81ed1373b07182bf25
mailmindlin <liamttlyrox@gmail.com>
2017-03-28 14:58:37 -0400

    Refactor VoidTypeTreeImpl

--- src/com/mindlin/jsast/impl/tree/VoidTypeTreeImpl.java
+++ src/com/mindlin/jsast/impl/tree/VoidTypeTreeImpl.java
@@ -3,21 +3,12 @@ package com.mindlin.jsast.impl.tree;
 import com.mindlin.jsast.impl.lexer.Token;
 import com.mindlin.jsast.tree.type.VoidTypeTree;
 
-public class VoidTypeTreeImpl extends AbstractTree implements VoidTypeTree {
-	protected final boolean implicit;
-
+public class VoidTypeTreeImpl extends AbstractTypeTree implements VoidTypeTree {
 	public VoidTypeTreeImpl(Token t) {
 		this(t.getStart(), t.getEnd(), false);
 	}
 
 	public VoidTypeTreeImpl(long start, long end, boolean implicit) {
-		super(start, end);
-		this.implicit = implicit;
+		super(start, end, implicit);
 	}
-
-	@Override
-	public boolean isImplicit() {
-		return implicit;
-	}
-
 }
