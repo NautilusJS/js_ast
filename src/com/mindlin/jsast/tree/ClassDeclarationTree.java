d24a102ebaba4928e80d2e792d6cc0fa47ed2f32
mailmindlin <mailmindlin@users.noreply.github.com>
2017-03-28 14:02:39 -0400

    Update ClassDeclarationTree.java

--- src/com/mindlin/jsast/tree/ClassDeclarationTree.java
+++ src/com/mindlin/jsast/tree/ClassDeclarationTree.java
@@ -4,14 +4,29 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ClassDeclarationTree extends ExpressionTree, StatementTree {
+	/**
+	 * Get class identifier name, else null if not set
+	 */
 	IdentifierTree getIdentifier();
 	
+	/**
+	 * Get the class that this class extends, if available
+	 */
 	Optional<TypeTree> getSuperType();
 	
+	/**
+	 * Get the interfaces that this class implements
+	 */
 	List<TypeTree> getImplementing();
 	
+	/**
+	 * Get the properties for this class
+	 */
 	List<ClassPropertyTree<?>> getProperties();
 	
+	/**
+	 * Get if this class is abstract
+	 */
 	boolean isAbstract();
 	
 	@Override
