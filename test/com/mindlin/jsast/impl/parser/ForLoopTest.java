e89208df92bbfcf7b0c2b9e93eeecee573e52162
mailmindlin <liamttlyrox@gmail.com>
2017-03-28 14:54:35 -0400

    Added more to for loop test

--- test/com/mindlin/jsast/impl/parser/ForLoopTest.java
+++ test/com/mindlin/jsast/impl/parser/ForLoopTest.java
@@ -5,12 +5,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.mindlin.jsast.tree.ForLoopTree;
 import com.mindlin.jsast.tree.StatementTree;
+import com.mindlin.jsast.tree.Tree;
 
 public class ForLoopTest {
 	@Test
-	public void testForLoops() {
-		StatementTree loop = parseStatement("for(var i in [1,2,3]);");
-		//System.out.println(loop);
+	public void testForInLoop() {
+		StatementTree loopStmt = parseStatement("for(var i in [1,2,3]);");
+		assertEquals(Tree.Kind.FOR_IN_LOOP, loopStmt.getKind());
+	}
+	
+	@Test
+	public void testForLoop() {
+		StatementTree loopStmt = parseStatement("for(var i = 0; i < 10; i++);");
+		assertEquals(Tree.Kind.FOR_LOOP, loopStmt.getKind());
+		ForLoopTree loop = (ForLoopTree) loopStmt;
+	}
+	
+	@Test
+	public void testForOfLoop() {
+		StatementTree loopStmt = parseStatement("for(var i of [1, 2, 3]);");
+		assertEquals(Tree.Kind.FOR_OF_LOOP, loopStmt.getKind());
 	}
 }
