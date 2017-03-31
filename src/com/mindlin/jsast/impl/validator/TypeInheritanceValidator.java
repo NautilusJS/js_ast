6f9d0e4945ca2e1a525ceb872473b32120c9a3b3
mailmindlin <liamttlyrox@gmail.com>
2017-03-31 01:43:14 -0400

    Support more type calculation

--- src/com/mindlin/jsast/impl/validator/TypeInheritanceValidator.java
+++ src/com/mindlin/jsast/impl/validator/TypeInheritanceValidator.java
@@ -3,6 +3,7 @@ package com.mindlin.jsast.impl.validator;
 import com.mindlin.jsast.tree.Tree;
 import com.mindlin.jsast.tree.TypeTree;
 import com.mindlin.jsast.tree.type.ArrayTypeTree;
+import com.mindlin.jsast.tree.type.IntersectionTypeTree;
 import com.mindlin.jsast.tree.type.UnionTypeTree;
 
 /**
@@ -13,9 +14,16 @@ public class TypeInheritanceValidator {
 		switch (base.getKind()) {
 			case ANY_TYPE:
 				return true;
+			case VOID_TYPE:
+				
 			case TYPE_UNION: {
 				UnionTypeTree union = (UnionTypeTree) base;
+				//TODO fix generic param binding
 				return canExtend(union.getLeftType(), child) || canExtend(union.getRightType(), child);
+			}
+			case TYPE_INTERSECTION: {
+				IntersectionTypeTree intersection = (IntersectionTypeTree) base;
+				return canExtend(intersection.getLeftType(), child) && canExtend(intersection.getRightType(), child);
 			}
 			case ARRAY_TYPE: {
 				if (child.getKind() != Tree.Kind.ARRAY_TYPE)
