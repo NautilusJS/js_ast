e4161efbc0c090d39f533f6f8ddc2f6f0e05b8ca
mailmindlin <liamttlyrox@gmail.com>
2017-03-31 01:44:55 -0400

    Add more lambda function parsing tests
    Turns out that parsing optional arguments is broken in some cases.

--- test/com/mindlin/jsast/impl/parser/LambdaTest.java
+++ test/com/mindlin/jsast/impl/parser/LambdaTest.java
@@ -2,10 +2,14 @@ package com.mindlin.jsast.impl.parser;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import com.mindlin.jsast.tree.ExpressionTree;
 import com.mindlin.jsast.tree.FunctionExpressionTree;
+import com.mindlin.jsast.tree.ParameterTree;
+import com.mindlin.jsast.tree.Tree;
 import com.mindlin.jsast.tree.Tree.Kind;
 
 import static com.mindlin.jsast.impl.parser.JSParserTest.*;
@@ -19,41 +23,115 @@ public class LambdaTest {
 		assertExceptionalExpression("(x,y)=>()", "Empty parentheses aren't a valid lambda body");
 		assertExceptionalExpression("...x=>()", "Rest parameters aren't allowed without parentheses");
 		assertExceptionalExpression("x:void=>()", "Types aren't allowed without parentheses");
+		assertExceptionalExpression("x?=>()", "Optional parameters not allowed without parentheses");
+		assertExceptionalExpression("(x?=5)=>()", "Initializers not allowed for optional parameters");
 	}
 	
 	@Test
 	public void testSingleParamNoParen() {
 		FunctionExpressionTree lambda = parseExpression("x=>x", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(1,params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertNull(param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
+		
+		//TODO check body
 	}
 	
 	@Test
 	public void testSingleParanWithParen() {
 		FunctionExpressionTree lambda = parseExpression("(x)=>x", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(1,params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertNull(param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
 	}
 	
 	@Test
 	public void testSingleRestParam() {
 		FunctionExpressionTree lambda = parseExpression("(...x)=>x", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(1,params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertTrue(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertNull(param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
 	}
 	
 	@Test
 	public void testMultipleParams() {
 		FunctionExpressionTree lambda = parseExpression("(x,y)=>x", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(2, params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertNull(param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
+		
+		ParameterTree param1 = params.get(1);
+		assertFalse(param1.isRest());
+		assertFalse(param1.isOptional());
+		assertNull(param1.getInitializer());
+		assertIdentifier("y", param1.getIdentifier());
 	}
 	
 	@Test
 	public void testBracketedBody() {
 		FunctionExpressionTree lambda = parseExpression("()=>{}", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(0, params.size());
 	}
 	
 	@Test
 	public void testMultiParamsWithRest() {
 		FunctionExpressionTree lambda = parseExpression("(x,...y)=>x", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(2, params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertNull(param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
+		
+		ParameterTree param1 = params.get(1);
+		assertTrue(param1.isRest());
+		assertFalse(param1.isOptional());
+		assertNull(param1.getInitializer());
+		assertIdentifier("y", param1.getIdentifier());
 	}
 	
 	@Test
 	public void testSingleParamWithType() {
 		FunctionExpressionTree lambda = parseExpression("(x:string)=>x", Kind.FUNCTION_EXPRESSION);
+
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(1, params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertNull(param0.getInitializer());
+		//TODO check type
+		assertIdentifier("x", param0.getIdentifier());
 	}
 	
 	@Test
@@ -64,6 +142,15 @@ public class LambdaTest {
 	@Test
 	public void testSingleParamDefaultValue() {
 		FunctionExpressionTree lambda = parseExpression("(x = 5)=>x", Kind.FUNCTION_EXPRESSION);
+
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(1, params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertLiteral(5, param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
 	}
 	
 	@Test
@@ -79,11 +166,58 @@ public class LambdaTest {
 	@Test
 	public void testMultipleParamsOptional() {
 		FunctionExpressionTree lambda = parseExpression("(x?, y?)=>x", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(2, params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertTrue(param0.isOptional());
+		assertNull(param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
+		
+		ParameterTree param1 = params.get(1);
+		assertFalse(param1.isRest());
+		assertTrue(param1.isOptional());
+		assertNull(param1.getInitializer());
+		assertIdentifier("y", param1.getIdentifier());
+		
+		
 		lambda = parseExpression("(x, y?)=>x", Kind.FUNCTION_EXPRESSION);
+
+		params = lambda.getParameters();
+		assertEquals(2, params.size());
+		
+		param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertNull(param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
+		
+		param1 = params.get(1);
+		assertFalse(param1.isRest());
+		assertTrue(param1.isOptional());
+		assertNull(param1.getInitializer());
+		assertIdentifier("y", param1.getIdentifier());
 	}
 	
 	@Test
 	public void testMultiParamsDefaultValue() {
 		FunctionExpressionTree lambda = parseExpression("(x = 5, y = 6)=>x", Kind.FUNCTION_EXPRESSION);
+		
+		List<ParameterTree> params = lambda.getParameters();
+		assertEquals(2, params.size());
+		
+		ParameterTree param0 = params.get(0);
+		assertFalse(param0.isRest());
+		assertFalse(param0.isOptional());
+		assertLiteral(5, param0.getInitializer());
+		assertIdentifier("x", param0.getIdentifier());
+		
+		ParameterTree param1 = params.get(1);
+		assertFalse(param1.isRest());
+		assertFalse(param1.isOptional());
+		assertLiteral(6, param1.getInitializer());
+		assertIdentifier("y", param1.getIdentifier());
 	}
 }
