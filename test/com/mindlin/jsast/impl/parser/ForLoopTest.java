7dc9bcb795a8c609c8b597c49861b955fdcc20ab
mailmindlin <liamttlyrox@gmail.com>
2017-04-04 18:04:48 -0400

    More stringent for loop testing

--- test/com/mindlin/jsast/impl/parser/ForLoopTest.java
+++ test/com/mindlin/jsast/impl/parser/ForLoopTest.java
@@ -5,27 +5,67 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.mindlin.jsast.tree.BinaryTree;
+import com.mindlin.jsast.tree.ForEachLoopTree;
 import com.mindlin.jsast.tree.ForLoopTree;
 import com.mindlin.jsast.tree.StatementTree;
 import com.mindlin.jsast.tree.Tree;
+import com.mindlin.jsast.tree.Tree.Kind;
+import com.mindlin.jsast.tree.UnaryTree;
+import com.mindlin.jsast.tree.VariableDeclarationTree;
+import com.mindlin.jsast.tree.VariableDeclaratorTree;
 
 public class ForLoopTest {
 	@Test
 	public void testForInLoop() {
-		StatementTree loopStmt = parseStatement("for(var i in [1,2,3]);");
-		assertEquals(Tree.Kind.FOR_IN_LOOP, loopStmt.getKind());
+		ForEachLoopTree loop = parseStatement("for(var i in [1,2,3]);", Kind.FOR_IN_LOOP);
+		
+		VariableDeclarationTree declaration = (VariableDeclarationTree) loop.getVariable();
+		assertFalse(declaration.isConst());
+		assertFalse(declaration.isScoped());
+		assertEquals(1, declaration.getDeclarations().size());
+		
+		VariableDeclaratorTree declarator = declaration.getDeclarations().get(0);
+		assertNull(declarator.getIntitializer());
+		assertIdentifier("i", declarator.getIdentifier());
+		
+		//TODO parse array
 	}
 	
 	@Test
 	public void testForLoop() {
-		StatementTree loopStmt = parseStatement("for(var i = 0; i < 10; i++);");
-		assertEquals(Tree.Kind.FOR_LOOP, loopStmt.getKind());
-		ForLoopTree loop = (ForLoopTree) loopStmt;
+		ForLoopTree loop = parseStatement("for(var i = 0; i < 10; i++);", Kind.FOR_LOOP);
+		
+		//TODO check initializer
+		
+		assertEquals(Kind.LESS_THAN, loop.getCondition().getKind());
+		BinaryTree condition = (BinaryTree) loop.getCondition();
+		assertIdentifier("i", condition.getLeftOperand());
+		assertLiteral(10, condition.getRightOperand());
+		
+		assertEquals(Kind.POSTFIX_INCREMENT, loop.getUpdate().getKind());
+		assertIdentifier("i", ((UnaryTree)loop.getUpdate()).getExpression());
+	}
+	
+	@Test
+	public void testForLoopEmpty() {
+		ForLoopTree loop = parseStatement("for(;;);", Kind.FOR_LOOP);
+		//Initializer not null (should be empty statement)
+		assertNull(loop.getCondition());
+		assertNull(loop.getUpdate());
 	}
 	
 	@Test
 	public void testForOfLoop() {
-		StatementTree loopStmt = parseStatement("for(var i of [1, 2, 3]);");
-		assertEquals(Tree.Kind.FOR_OF_LOOP, loopStmt.getKind());
+		ForEachLoopTree loop = parseStatement("for(var i of [1, 2, 3]);", Kind.FOR_OF_LOOP);
+		
+		VariableDeclarationTree declaration = (VariableDeclarationTree) loop.getVariable();
+		assertFalse(declaration.isConst());
+		assertFalse(declaration.isScoped());
+		assertEquals(1, declaration.getDeclarations().size());
+		
+		VariableDeclaratorTree declarator = declaration.getDeclarations().get(0);
+		assertNull(declarator.getIntitializer());
+		assertIdentifier("i", declarator.getIdentifier());
 	}
 }
