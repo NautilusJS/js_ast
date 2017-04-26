5d0ff2724edd664bd78d1a841d96e71d47d4c47d
mailmindlin <liamttlyrox@gmail.com>
2017-04-25 22:31:52 -0400

    Prettified REPL Ux

--- example/com/mindlin/jsast/example/REPL.java
+++ example/com/mindlin/jsast/example/REPL.java
@@ -1,18 +1,29 @@
 package com.mindlin.jsast.example;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Scanner;
 
 import com.mindlin.jsast.impl.parser.JSParser;
+import com.mindlin.jsast.impl.writer.JSWriterImpl;
 import com.mindlin.jsast.tree.CompilationUnitTree;
+import com.mindlin.jsast.writer.JSWriterOptions;
 
 public class REPL {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		JSParser parser = new JSParser();
+		JSWriterOptions options = new JSWriterOptions();
+		options.indentStyle = "\t";
+		JSWriterImpl writer = new JSWriterImpl(options);
 		Scanner s = new Scanner(System.in);
+		System.out.println("Nautilus JS transpiler");
 		while (true) {
+			System.out.print(">>> ");
 			String line = s.nextLine();
 			CompilationUnitTree ast = parser.apply("tmp", line);
-			System.out.println(ast);
+			StringWriter out = new StringWriter();
+			writer.write(ast, out);
+			System.out.println(out.toString());
 		}
 	}
 }
