739ff88e4ead3f20f846345f9e090afbd09cb488
mailmindlin <liamttlyrox@gmail.com>
2017-05-01 22:49:09 -0400

    More type inheritance magic

--- src/com/mindlin/jsast/impl/validator/TypeInheritanceValidator.java
+++ src/com/mindlin/jsast/impl/validator/TypeInheritanceValidator.java
@@ -7,7 +7,10 @@ import com.mindlin.jsast.tree.Tree;
 import com.mindlin.jsast.tree.Tree.Kind;
 import com.mindlin.jsast.tree.TypeTree;
 import com.mindlin.jsast.tree.type.ArrayTypeTree;
+import com.mindlin.jsast.tree.type.IdentifierTypeTree;
 import com.mindlin.jsast.tree.type.IntersectionTypeTree;
+import com.mindlin.jsast.tree.type.SpecialTypeTree;
+import com.mindlin.jsast.tree.type.SpecialTypeTree.SpecialType;
 import com.mindlin.jsast.tree.type.UnionTypeTree;
 
 /**
@@ -17,6 +20,28 @@ public class TypeInheritanceValidator {
 	public static boolean canExtend(ASTContext context, TypeTree base, TypeTree child) {
 		switch (base.getKind()) {
 			case SPECIAL_TYPE: {
+				SpecialType type = ((SpecialTypeTree) base).getType();
+				if (child.getKind() == Kind.SPECIAL_TYPE) {
+					SpecialType childType = ((SpecialTypeTree) child).getType();
+					if (type == childType)
+						return true;
+					
+					switch (type) {
+						case ANY:
+							return true;
+							
+						case UNDEFINED:
+						case NEVER:
+							return false;
+					}
+				}
+				switch (type) {
+					case ANY:
+						return true;
+					case VOID:
+					case NEVER:
+						return child.getKind() == Kind.SPECIAL_TYPE && ((SpecialTypeTree) child).getType() == SpecialType.NEVER;
+				}
 				return true;
 			}
 			case TYPE_UNION: {
@@ -38,6 +63,20 @@ public class TypeInheritanceValidator {
 		}
 	}
 	
+	public static TypeTree computeMemberType(TypeContext context, TypeTree baseType, String memberName) {
+		switch (baseType.getKind()) {
+			case IDENTIFIER_TYPE:
+				baseType = context.resolve((IdentifierTypeTree)baseType);
+				break;
+		}
+		
+		return null;
+	}
+	
+	public static TypeTree computeIndexType(TypeTree baseType, TypeTree indexType) {
+		return null;
+	}
+	
 	public static TypeTree computeExpressionType(ASTContext context, ExpressionTree expression) {
 		switch (expression.getKind()) {
 			case IDENTIFIER:
@@ -52,6 +91,9 @@ public class TypeInheritanceValidator {
 	}
 	
 	public static TypeTree reduce(ASTContext context, TypeTree type) {
+		if (type.getKind() == Kind.TYPE_UNION) {
+			
+		}
 		return type;
 	}
 }
