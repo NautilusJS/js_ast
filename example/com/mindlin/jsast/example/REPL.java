e97d327c9f523378437731f3955b740f094503f3
mailmindlin <liamttlyrox@gmail.com>
2017-05-08 23:02:30 -0400

    Transform REPL output

--- example/com/mindlin/jsast/example/REPL.java
+++ example/com/mindlin/jsast/example/REPL.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 import com.mindlin.jsast.impl.parser.JSParser;
 import com.mindlin.jsast.impl.writer.JSWriterImpl;
+import com.mindlin.jsast.transform.ASTTransformer;
+import com.mindlin.jsast.transform.ASTTransformerContext;
+import com.mindlin.jsast.transform.ExpressionFlattenerTransformation;
 import com.mindlin.jsast.tree.CompilationUnitTree;
 import com.mindlin.jsast.writer.JSWriterOptions;
 
@@ -15,7 +18,7 @@ public class REPL {
 		JSWriterOptions options = new JSWriterOptions();
 		options.indentStyle = "\t";
 		JSWriterImpl writer = new JSWriterImpl(options);
-		//ASTTransformation transformer = new ExpressionFlattenerTransformation();
+		ASTTransformer<ASTTransformerContext> transformer = new ASTTransformer<>(new ExpressionFlattenerTransformation());
 		Scanner s = new Scanner(System.in);
 		System.out.println("Nautilus JS transpiler");
 		while (true) {
@@ -32,15 +35,16 @@ public class REPL {
 				}
 			}
 			CompilationUnitTree ast = parser.apply("tmp", sb.toString());
+//			System.out.print(ast);
 			StringWriter out = new StringWriter();
 			writer.write(ast, out);
 			System.out.println(out.toString());
 			
-			//ast = (CompilationUnitTree) ast.accept(transformer, null);
-			//out = new StringWriter();
-			//writer.write(ast, out);
+			ast = (CompilationUnitTree) ast.accept(transformer, null);
+			out = new StringWriter();
+			writer.write(ast, out);
 			System.out.println(out.toString());
-			//System.err.println(ast);
+//			System.err.println(ast);
 		}
 	}
 }
