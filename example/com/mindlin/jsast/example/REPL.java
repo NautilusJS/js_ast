892a18a9e25cfa2626d6322416469220e05d4365
mailmindlin <liamttlyrox@gmail.com>
2017-09-01 15:38:11 -0400

    (Attempt to) evaluate code in REPL

--- example/com/mindlin/jsast/example/REPL.java
+++ example/com/mindlin/jsast/example/REPL.java
@@ -5,6 +5,7 @@ import java.io.StringWriter;
 import java.util.Scanner;
 
 import com.mindlin.jsast.impl.parser.JSParser;
+import com.mindlin.jsast.impl.runtime.JSScriptEngine;
 import com.mindlin.jsast.impl.writer.JSWriterImpl;
 import com.mindlin.jsast.transform.DeadCodeRemovalTransformation;
 import com.mindlin.jsast.transform.ExpressionFlattenerTransformation;
@@ -19,7 +20,9 @@ public class REPL {
 		options.indentStyle = "\t";
 		JSWriterImpl writer = new JSWriterImpl(options);
 		TransformerSeries transformer = new TransformerSeries(new ExpressionFlattenerTransformation(), new DeadCodeRemovalTransformation());
+		@SuppressWarnings("resource")
 		Scanner s = new Scanner(System.in);
+		JSScriptEngine engine = new JSScriptEngine();
 		System.out.println("Nautilus JS transpiler");
 		while (true) {
 			StringBuffer sb = new StringBuffer();
@@ -44,7 +47,13 @@ public class REPL {
 			out = new StringWriter();
 			writer.write(ast, out);
 			System.out.println(out.toString());
-			System.err.println(ast);
+			System.out.println(ast);
+			
+			try {
+				System.out.println(engine.eval(sb.toString()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
