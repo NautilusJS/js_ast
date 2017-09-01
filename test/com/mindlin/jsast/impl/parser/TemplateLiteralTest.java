3ffa731376e1229f214e805c37783ad1420294fa
mailmindlin <liamttlyrox@gmail.com>
2017-09-01 15:33:37 -0400

    Better template literal testing

--- test/com/mindlin/jsast/impl/parser/TemplateLiteralTest.java
+++ test/com/mindlin/jsast/impl/parser/TemplateLiteralTest.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.mindlin.jsast.tree.BinaryTree;
+import com.mindlin.jsast.tree.ExpressionTree;
 import com.mindlin.jsast.tree.TemplateElementTree;
 import com.mindlin.jsast.tree.TemplateLiteralTree;
 import com.mindlin.jsast.tree.Tree.Kind;
@@ -17,20 +19,34 @@ public class TemplateLiteralTest {
 	@Test
 	public void testTemplateString() {
 		TemplateLiteralTree template = parseExpression("`Hello`", Kind.TEMPLATE_LITERAL);
-		System.out.println(template);
+		
 		List<TemplateElementTree> quasis = template.getQuasis();
 		assertEquals(quasis.size(), 1);
+		
+		TemplateElementTree t0 = quasis.get(0);
+		assertEquals("Hello", t0.getCooked());
 	}
 	
 	@Test
 	public void testTemplateOnlyExpr() {
-		TemplateLiteralTree template = parseExpression("`${1+1}`", Kind.TEMPLATE_LITERAL);
-		System.out.println(template);
+		TemplateLiteralTree template = parseExpression("`${1+2}`", Kind.TEMPLATE_LITERAL);
+
+		List<TemplateElementTree> quasis = template.getQuasis();
+		assertEquals(quasis.size(), 2);
+		assertEquals("", quasis.get(0).getCooked());
+		assertEquals("", quasis.get(1).getCooked());
+		
+		List<ExpressionTree> exprs = template.getExpressions();
+		assertEquals(exprs.size(), 1);
+		BinaryTree expr = assertKind(exprs.get(0), Kind.ADDITION);
+		assertLiteral(1, expr.getLeftOperand());
+		assertLiteral(2, expr.getRightOperand());
 	}
 	
 	@Test
 	public void testTemplateComplex() {
 		TemplateLiteralTree template = parseExpression("`Hello, ${'world!'}`", Kind.TEMPLATE_LITERAL);
-		System.out.println(template);
+		
+		//TODO check
 	}
 }
