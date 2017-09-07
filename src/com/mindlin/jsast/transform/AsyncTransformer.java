8526c7392b879d2fb205cc0339b25d8027c6a332
mailmindlin <liamttlyrox@gmail.com>
2017-09-07 17:29:55 -0400

    Bugfix
    Honestly, can't remember how it was going to work.
    Maybe something stochastic?

--- src/com/mindlin/jsast/transform/AsyncTransformer.java
+++ src/com/mindlin/jsast/transform/AsyncTransformer.java
@@ -4,12 +4,11 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import com.mindlin.jsast.impl.util.MultiValuedMap;
 import com.mindlin.jsast.tree.Tree;
 
 public class AsyncTransformer {
 	Queue<Tree> queue = new LinkedBlockingQueue<>();
-	MultiValuedMap<? extends Tree, TreeReplacement> observers = new MultiValuedMap<>();
+	//MultiValuedMap<? extends Tree, TreeReplacement> observers = new MultiValuedMap<>();
 	
 	interface TreeReplacement {
 		Tree base();
