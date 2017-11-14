6595fd08455f4aec6144ba75207551cedc1c8e94
mailmindlin <liamttlyrox@gmail.com>
2017-11-14 14:55:01 -0500

    Inheritance fix

--- src/com/mindlin/jsast/impl/tree/ExportTreeImpl.java
+++ src/com/mindlin/jsast/impl/tree/ExportTreeImpl.java
@@ -3,7 +3,7 @@ package com.mindlin.jsast.impl.tree;
 import com.mindlin.jsast.tree.ExportTree;
 import com.mindlin.jsast.tree.ExpressionTree;
 
-public class ExportTreeImpl extends AbstractExpressiveExpressionTree implements ExportTree {
+public class ExportTreeImpl extends AbstractExpressiveStatementTree implements ExportTree {
 	
 	public ExportTreeImpl(long start, long end, ExpressionTree expression) {
 		super(start, end, expression);
