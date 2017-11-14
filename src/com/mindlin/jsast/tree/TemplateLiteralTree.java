71ded765dfb669a9f3f72384e887ed8319ba146b
mailmindlin <liamttlyrox@gmail.com>
2017-11-14 10:01:42 -0500

    Template literals aren't object property keys

--- src/com/mindlin/jsast/tree/TemplateLiteralTree.java
+++ src/com/mindlin/jsast/tree/TemplateLiteralTree.java
@@ -2,17 +2,31 @@ package com.mindlin.jsast.tree;
 
 import java.util.List;
 
-public interface TemplateLiteralTree extends LiteralTree<String>, ObjectPropertyKeyTree {
+/**
+ * Template literal.
+ * 
+ * @author mailmindlin
+ *
+ */
+public interface TemplateLiteralTree extends LiteralTree<String> {
 	
+	/**
+	 * The actual strings in this template literal
+	 * @return list of quasis, in order
+	 */
 	List<TemplateElementTree> getQuasis();
 	
+	/**
+	 * Expressions interleaved between quasis
+	 * @return list of expressions, in order
+	 */
 	List<ExpressionTree> getExpressions();
 	
 	@Override
 	default Tree.Kind getKind() {
 		return Tree.Kind.TEMPLATE_LITERAL;
 	}
-
+	
 	@Override
 	default <R, D> R accept(ExpressionTreeVisitor<R, D> visitor, D data) {
 		return visitor.visitTemplateLiteral(this, data);
