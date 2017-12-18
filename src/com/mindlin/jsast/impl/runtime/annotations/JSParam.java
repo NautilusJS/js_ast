3aaf6ea5552d1d425a33aea297d80393cb3f664d
mailmindlin <liamttlyrox@gmail.com>
2017-12-18 18:21:13 -0500

    Keep JSParam anntations in runtime

--- src/com/mindlin/jsast/impl/runtime/annotations/JSParam.java
+++ src/com/mindlin/jsast/impl/runtime/annotations/JSParam.java
@@ -1,10 +1,14 @@
 package com.mindlin.jsast.impl.runtime.annotations;
 
 import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Target(PARAMETER)
+@Retention(RUNTIME)
 public @interface JSParam {
 	boolean optional() default false;
-	String defaultValue() default "";
+	String defaultValue() default "undefined";
 }
