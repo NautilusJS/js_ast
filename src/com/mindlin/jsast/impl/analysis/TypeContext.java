fa312dd668865d3e6727a1a37c43a43c10915445
mailmindlin <liamttlyrox@gmail.com>
2017-12-18 18:09:15 -0500

    Update TypeContext for new type system

--- src/com/mindlin/jsast/impl/analysis/TypeContext.java
+++ src/com/mindlin/jsast/impl/analysis/TypeContext.java
@@ -1,12 +1,33 @@
 package com.mindlin.jsast.impl.analysis;
 
+import java.util.Collections;
 import java.util.List;
 
-import com.mindlin.jsast.tree.type.IdentifierTypeTree;
-import com.mindlin.jsast.tree.type.TypeTree;
+import com.mindlin.jsast.type.Type;
 
+/**
+ * A readonly context that resolves types
+ * 
+ * @author mailmindlin
+ *
+ */
 public interface TypeContext {
-	TypeTree resolve(IdentifierTypeTree type);
-	TypeTree resolve(String name);
-	List<TypeTree> resolveFunction(String name);
+	default Type getType(String name) {
+		return getType(name, Collections.emptyList());
+	}
+	
+	Type getType(String name, List<Type> generics);
+	
+	Type resolveAliases(Type type);
+	
+	//Type variables
+	Type thisType();
+	
+	Type superType();
+	
+	Type argumentsType();
+	
+	default Type intern(Type t) {
+		return t;
+	}
 }
