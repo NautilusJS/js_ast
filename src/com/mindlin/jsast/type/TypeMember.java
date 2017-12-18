9808828e20e0752670434e39d62d1e270c1230ec
mailmindlin <liamttlyrox@gmail.com>
2017-12-18 18:16:24 -0500

    TypeMembers have Type keys
    For ES6 maps. They're a pain.

--- src/com/mindlin/jsast/type/TypeMember.java
+++ src/com/mindlin/jsast/type/TypeMember.java
@@ -1,12 +1,15 @@
 package com.mindlin.jsast.type;
 
+import com.mindlin.jsast.tree.ClassPropertyTree.AccessModifier;
+
 public class TypeMember {
+	AccessModifier access;
 	boolean required = true;
 	boolean readonly = false;
-	String key;
-	Object value;
+	Type key;
+	Type value;
 	
-	public TypeMember(boolean required, boolean readonly, String key, Type value) {
+	public TypeMember(boolean required, boolean readonly, Type key, Type value) {
 		this.required = required;
 		this.readonly = readonly;
 		this.key = key;
@@ -21,11 +24,35 @@ public class TypeMember {
 		return this.readonly;
 	}
 	
-	public String getName() {
+	public Type getName() {
 		return this.key;
 	}
 	
 	public Type getType() {
-		return null;
+		return this.value;
+	}
+	
+	public AccessModifier getAccess() {
+		return this.access;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(super.toString());
+		sb.append('{');
+		
+		if (this.isReadonly())
+			sb.append("readonly ");
+		
+		sb.append(this.getName());
+		
+		if (!this.isRequired())
+			sb.append('?');
+		
+		if (this.getType() != null)
+			sb.append(": ").append(this.getType());
+		
+		sb.append('}');
+		return sb.toString();
 	}
 }
