a83c7309b2ed6f9106a23be6f51254715a326442
mailmindlin <mailmindlin@users.noreply.github.com>
2018-11-19 16:33:26 -0500

    Fix class changed methods in test

--- test/com/mindlin/jsast/impl/parser/ClassDeclarationTest.java
+++ test/com/mindlin/jsast/impl/parser/ClassDeclarationTest.java
@@ -1,11 +1,13 @@
 package com.mindlin.jsast.impl.parser;
 
-import static com.mindlin.jsast.impl.parser.JSParserTest.*;
+import static com.mindlin.jsast.impl.parser.JSParserTest.assertIdentifier;
+import static com.mindlin.jsast.impl.parser.JSParserTest.parseExpression;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import com.mindlin.jsast.tree.ClassDeclarationTree;
+import com.mindlin.jsast.tree.Modifiers;
+import com.mindlin.jsast.tree.ClassTreeBase.ClassDeclarationTree;
 import com.mindlin.jsast.tree.Tree.Kind;
 
 public class ClassDeclarationTest {
@@ -13,51 +15,51 @@ public class ClassDeclarationTest {
 	@Test
 	public void testSimpleClassDeclaration() {
 		ClassDeclarationTree clazz = parseExpression("class Foo{}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
-		assertFalse(clazz.isAbstract());
+		assertIdentifier("Foo", clazz.getName());
+		assertEquals(clazz.getModifiers(), Modifiers.NONE);
 	}
 	
 	@Test
 	public void testEmptyAbstractClass() {
 		ClassDeclarationTree clazz = parseExpression("abstract class Foo {}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
-		assertTrue(clazz.isAbstract());
+		assertIdentifier("Foo", clazz.getName());
+		assertEquals(clazz.getModifiers(), Modifiers.ABSTRACT);
 	}
 	
 	@Test
 	public void testClassExtends() {
 		ClassDeclarationTree clazz = parseExpression("class Foo extends Bar {}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
+		assertIdentifier("Foo", clazz.getName());
 		//TODO check superclass
 	}
 	
 	@Test
 	public void testClassImplements() {
 		ClassDeclarationTree clazz = parseExpression("class Foo implements Bar {}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
+		assertIdentifier("Foo", clazz.getName());
 	}
 	
 	@Test
 	public void testClassImplementsMultiple() {
 		ClassDeclarationTree clazz = parseExpression("class Foo implements Bar, Baz {}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
+		assertIdentifier("Foo", clazz.getName());
 	}
 	
 	@Test
 	public void testClassGenerics() {
 		ClassDeclarationTree clazz = parseExpression("class Foo<T> {}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
+		assertIdentifier("Foo", clazz.getName());
 	}
 	
 	@Test
 	public void testClassMembers() {
 		ClassDeclarationTree clazz = parseExpression("class Foo {bar;}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
+		assertIdentifier("Foo", clazz.getName());
 	}
 	
 	@Test
 	public void testClassAbstractProperties() {
 		ClassDeclarationTree clazz = parseExpression("abstract class Foo {abstract bar();}", Kind.CLASS_DECLARATION);
-		assertIdentifier("Foo", clazz.getIdentifier());
+		assertIdentifier("Foo", clazz.getName());
 	}
 }
