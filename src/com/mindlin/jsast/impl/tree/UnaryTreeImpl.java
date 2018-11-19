6a426dad40754ab756305b531f9efc600339fd66
mailmindlin <mailmindlin@users.noreply.github.com>
2018-11-19 12:50:01 -0500

    Remove AbstractExpressiveExpressionTree
    Increases our LOC, but I think it increases readability

--- src/com/mindlin/jsast/impl/tree/UnaryTreeImpl.java
+++ src/com/mindlin/jsast/impl/tree/UnaryTreeImpl.java
@@ -1,19 +1,33 @@
 package com.mindlin.jsast.impl.tree;
 
+import java.util.Objects;
+
 import com.mindlin.jsast.fs.SourcePosition;
 import com.mindlin.jsast.tree.ExpressionTree;
 import com.mindlin.jsast.tree.UnaryTree;
 
-public class UnaryTreeImpl extends AbstractExpressiveExpressionTree implements UnaryTree {
+public class UnaryTreeImpl extends AbstractTree implements UnaryTree {
+	protected final ExpressionTree expression;
 	protected final Kind kind;
 	
 	public UnaryTreeImpl(SourcePosition start, SourcePosition end, ExpressionTree expression, Kind kind) {
-		super(start, end, expression);
+		super(start, end);
+		this.expression = expression;
 		this.kind = kind;
 	}
 	
 	@Override
 	public Kind getKind() {
 		return this.kind;
+	}
+
+	@Override
+	public ExpressionTree getExpression() {
+		return this.expression;
+	}
+	
+	@Override
+	protected int hash() {
+		return Objects.hash(getKind(), getExpression());
 	}
 }
