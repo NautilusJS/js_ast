f7c36cdd1f8d4e4ff46b49db90cba273ef2e586d
mailmindlin <mailmindlin@users.noreply.github.com>
2018-11-19 16:46:56 -0500

    More Token predicates

--- src/com/mindlin/jsast/impl/parser/TokenPredicate.java
+++ src/com/mindlin/jsast/impl/parser/TokenPredicate.java
@@ -14,18 +14,34 @@ public class TokenPredicate {
 		return t -> t.matches(kind, value);
 	}
 	
-	public static Predicate<Token> VARIABLE_START = t->(t.isKeyword() && (t.getValue() == JSKeyword.VAR || t.getValue() == JSKeyword.LET || t.getValue() == JSKeyword.CONST));
-	public static Predicate<Token> LET_OR_CONST = t->(t.isKeyword() && (t.getValue() == JSKeyword.LET || t.getValue() == JSKeyword.CONST));
-	public static Predicate<Token> IN_OR_OF = t->(t.isKeyword() && (t.getValue() == JSKeyword.IN || t.getValue() == JSKeyword.OF));
-	public static Predicate<Token> PARAMETER_TYPE_START = t->(t.isOperator() && (t.getValue() == JSOperator.QUESTION_MARK || t.getValue() == JSOperator.COLON || t.getValue() == JSOperator.ASSIGNMENT));
-	public static Predicate<Token> START_OF_PARAMETER = t->{
+	public static final Predicate<Token> VARIABLE_START = t->(t.isKeyword() && (t.getValue() == JSKeyword.VAR || t.getValue() == JSKeyword.LET || t.getValue() == JSKeyword.CONST));
+	public static final Predicate<Token> IN_OR_OF = t->(t.isKeyword() && (t.getValue() == JSKeyword.IN || t.getValue() == JSKeyword.OF));
+	public static final Predicate<Token> PARAMETER_TYPE_START = t->(t.isOperator() && (t.getValue() == JSOperator.QUESTION_MARK || t.getValue() == JSOperator.COLON || t.getValue() == JSOperator.ASSIGNMENT));
+	public static final Predicate<Token> START_OF_PARAMETER = t->{
 		if (t.isIdentifier())
 			return true;
 		if (t.isBracket())
 			return t.getValue().equals('{') || t.getValue().equals('[');
 		return t.matches(TokenKind.OPERATOR, JSOperator.SPREAD);
 	};
-	public static Predicate<Token> TYPE_CONTINUATION = t->(t.isOperator() && (t.getValue() == JSOperator.BITWISE_OR || t.getValue() == JSOperator.BITWISE_AND));
+	public static final Predicate<Token> TYPE_CONTINUATION = t->(t.isOperator() && (t.getValue() == JSOperator.BITWISE_OR || t.getValue() == JSOperator.BITWISE_AND));
+	public static final Predicate<Token> CAN_FOLLOW_MODIFIER = t -> {
+		Object value = t.getValue();
+		switch (t.getKind()) {
+			case BRACKET:
+				return (char) value == '[' || (char) value == '{';
+			case OPERATOR:
+				return value == JSOperator.MULTIPLICATION || value == JSOperator.SPREAD;
+			case STRING_LITERAL:
+			case NUMERIC_LITERAL:
+			case IDENTIFIER:
+			case KEYWORD:
+				return true;
+			default:
+				return false;
+		}
+	};
+	public static final Predicate<Token> HERITAGE_START = t -> (t.isKeyword() && (t.getValue() == JSKeyword.EXTENDS || t.getValue() == JSKeyword.IMPLEMENTS));
 	/**
 	 * TokenPredicate can't be instantiated
 	 */
