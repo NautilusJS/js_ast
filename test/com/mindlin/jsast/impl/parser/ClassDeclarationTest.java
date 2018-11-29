9264ff08ef3a71470514a5330ff806f1af4c5b2c
mailmindlin <mailmindlin@users.noreply.github.com>
2018-11-29 08:37:41 -0500

    Validate more of parsed classes

--- test/com/mindlin/jsast/impl/parser/ClassDeclarationTest.java
+++ test/com/mindlin/jsast/impl/parser/ClassDeclarationTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 
 import com.mindlin.jsast.tree.Modifiers;
 import com.mindlin.jsast.tree.ClassTreeBase.ClassDeclarationTree;
-import com.mindlin.jsast.tree.ClassTreeBase.ClassExpressionTree;
+import com.mindlin.jsast.tree.HeritageClauseTree;
+import com.mindlin.jsast.tree.HeritageExpressionTree;
 import com.mindlin.jsast.tree.Tree.Kind;
+import com.mindlin.jsast.tree.type.TypeParameterDeclarationTree;
 
 public class ClassDeclarationTest {
 	
@@ -30,25 +32,46 @@ public class ClassDeclarationTest {
 	public void testClassExtends() {
 		ClassDeclarationTree clazz = parseStatement("class Foo extends Bar {}", Kind.CLASS_DECLARATION);
 		assertIdentifier("Foo", clazz.getName());
-		//TODO check superclass
+		
+		HeritageClauseTree parent = assertSingleElementKind(Kind.EXTENDS_CLAUSE, clazz.getHeritage());
+		
+		HeritageExpressionTree superTarget = assertSingleElementKind(Kind.HERITAGE_EXPRESSION, parent.getTypes());
+		assertIdentifier("Bar", superTarget.getExpression());
 	}
 	
 	@Test
 	public void testClassImplements() {
 		ClassDeclarationTree clazz = parseStatement("class Foo implements Bar {}", Kind.CLASS_DECLARATION);
 		assertIdentifier("Foo", clazz.getName());
+		
+		HeritageClauseTree impl = assertSingleElementKind(Kind.IMPLEMENTS_CLAUSE, clazz.getHeritage());
+		
+		HeritageExpressionTree implTarget = assertSingleElementKind(Kind.HERITAGE_EXPRESSION, impl.getTypes());
+		assertIdentifier("Bar", implTarget.getExpression());
 	}
 	
 	@Test
 	public void testClassImplementsMultiple() {
 		ClassDeclarationTree clazz = parseStatement("class Foo implements Bar, Baz {}", Kind.CLASS_DECLARATION);
 		assertIdentifier("Foo", clazz.getName());
+		
+		HeritageClauseTree impl = assertSingleElementKind(Kind.IMPLEMENTS_CLAUSE, clazz.getHeritage());
+		assertEquals(2, impl.getTypes().size());
+		
+		HeritageExpressionTree implTarget0 = impl.getTypes().get(0);
+		assertIdentifier("Bar", implTarget0.getExpression());
+		
+		HeritageExpressionTree implTarget1 = impl.getTypes().get(1);
+		assertIdentifier("Baz", implTarget1.getExpression());
 	}
 	
 	@Test
-	public void testClassGenerics() {
+	public void testClassWithTypeParameters() {
 		ClassDeclarationTree clazz = parseStatement("class Foo<T> {}", Kind.CLASS_DECLARATION);
 		assertIdentifier("Foo", clazz.getName());
+		
+		TypeParameterDeclarationTree tParam0 = assertSingleElementKind(Kind.TYPE_PARAMETER_DECLARATION, clazz.getTypeParameters());
+		assertIdentifier("T", tParam0.getName());
 	}
 	
 	@Test
