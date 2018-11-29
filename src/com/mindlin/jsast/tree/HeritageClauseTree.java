c37d9f71c44cac380fd4b83e95866389dee831b8
mailmindlin <mailmindlin@users.noreply.github.com>
2018-11-29 08:50:29 -0500

    Implement Heritage

--- src/com/mindlin/jsast/tree/HeritageClauseTree.java
+++ src/com/mindlin/jsast/tree/HeritageClauseTree.java
@@ -2,10 +2,12 @@ package com.mindlin.jsast.tree;
 
 import java.util.List;
 
+import com.mindlin.jsast.tree.Tree.Kind;
+
 /**
  * Extends/implements clause
  * @author mailmindlin
  */
 public interface HeritageClauseTree extends Tree, UnvisitableTree {
-	List<Tree> getTypes();
+	List<HeritageExpressionTree> getTypes();
 }
