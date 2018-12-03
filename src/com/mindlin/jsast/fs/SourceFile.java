9255853004d864d9ee08e9b2dfd547e425e824e3
mailmindlin <mailmindlin@users.noreply.github.com>
2018-12-03 12:19:35 -0500

    Fix import

--- src/com/mindlin/jsast/fs/SourceFile.java
+++ src/com/mindlin/jsast/fs/SourceFile.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.util.Arrays;
 
 import com.mindlin.jsast.impl.tree.LineMap;
+import com.mindlin.jsast.impl.util.CharacterArrayStream;
 import com.mindlin.jsast.impl.util.CharacterStream;
 
 /**
